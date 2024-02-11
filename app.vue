fbc91576bb83011f3022606fcdbeb178e44e6218
abstrucked <mxhgraphics@gmail.com>
2024-02-11 16:47:55 +0100

    initial commit

---
+++ app.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+import frameConfig from "./frame.config.json"
+
+useHead({
+  title: 'Nuxt Frame Template',
+  meta: [
+    { name: 'description', content: 'My amazing site.' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { name: 'og:title', content: `${frameConfig.title}`},
+    { name: 'og:description', content: `${frameConfig.description}`},
+    { name: 'og:image', content: `${frameConfig.image}`},
+    { name: 'og:url', content: `${frameConfig.url}`},
+    { name: 'og:type', content: 'website'},
+    { name: 'og:site_name', content: `${frameConfig.title}`}
+  ],
+  bodyAttrs: {
+    class: 'test'
+  },
+  script: [ { innerHTML: 'console.log(\'Hello world\')' } ]
+})
+</script>
+<template>
+  <div>
+  </div>
+</template>
