18e49c2fdc4862b90ea361bbe31011b80cceaceb
abstrucked <mxhgraphics@gmail.com>
2024-02-11 18:56:43 +0100

    switched form string metadata to object for the useHead composable

--- app.vue
+++ app.vue
@@ -2,31 +2,28 @@
 
 import frameConfig from "./frame.config.json"
 
+const {
+  data
+} = useFetch("/api/test")
+
+
+
+
 useHead({
   title: 'Nuxt Frame Template',
-  meta: [
-    { name: 'description', content: 'My amazing site.' },
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-    { name: 'og:title', content: `${frameConfig.title}`},
-    { name: 'og:description', content: `${frameConfig.description}`},
-    { name: 'og:image', content: `${frameConfig.image}`},
-    { name: 'og:url', content: `${frameConfig.url}`},
-    { name: 'og:type', content: 'website'},
-    { name: 'og:site_name', content: `${frameConfig.title}`}
-  ],
+  meta: data ? [
+    { name: "title", content: "myFrame" }
+  ] : [],
   bodyAttrs: {
     class: 'test'
   },
   script: [ { innerHTML: 'console.log(\'Hello world\')' } ]
 })
 
-const {
-  data
-} = useFetch("/api/test")
+
 
 </script>
 <template>
   <div>
-    {{ data.body }}
   </div>
 </template>
--- server/api/test.ts
+++ server/api/test.ts
@@ -1,16 +1,32 @@
+import frameConfig from "~/frame.config.json";
+declare type Meta = {
+  name: string;
+  content: string;
+}
 const defaultMeta = {
   title: "Default Title",
   description: "Default Description",
   imgURL: "https://www.philmohun.com/content/images/size/w960/2022/12/farcaster-banner.png"
 }
+
+// const meta: [
+//   { name: 'description', content: 'My amazing site.' },
+//   { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+//   { name: 'og:title', content: `${frameConfig.title}`},
+//   { name: 'og:description', content: `${frameConfig.description}`},
+//   { name: 'og:image', content: `${frameConfig.image}`},
+//   { name: 'og:url', content: `${frameConfig.url}`},
+//   { name: 'og:type', content: 'website'},
+//   { name: 'og:site_name', content: `${frameConfig.title}`}
+// ]
 class FrameButton {
   constructor(public label: string, public action: string, public target: string, public index: number) {}
-  get html(): string {
-    return `
-        <meta property="fc:frame:button:${this.index}" content="${this.label}">
-        <meta property="fc:button:${this.index}:action"  content="${this.action}">
-        <meta property="fc:button:${this.index}:target" content="${this.target}">
-    `
+  getMeta(): Array<Meta> {
+    return [
+      { name: `fc:frame:button:${this.index}`, content: this.label },
+      { name: `fc:button:${this.index}:action`, content: this.action },
+      { name: `fc:button:${this.index}:target`, content: this.target }
+    ]
   }
 }
 const farcasterMeta = {
@@ -21,30 +37,42 @@ const farcasterMeta = {
     new FrameButton("Frame Docs", "link", "https://docs.farcaster.xyz/learn/what-is-farcaster/frames#frames", 3)
   ]
 }
-const _html: string = `
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title>${defaultMeta.title}</title>
-        <meta property="og:title" content=${defaultMeta.title}>
-        <meta property="og:description" content=${defaultMeta.description}>
-        <meta property="og:image" content=${defaultMeta.imgURL}>
-        <!------------------ CUSTOM META TAGS --------------------------------->
-        <meta property="fc:frame" content="${farcasterMeta.frame}">
-        <meta property="fc:frame:image" content=${defaultMeta.imgURL}>
-        ${farcasterMeta.buttons.map((button) => button.html).join("\n")}
-     </head>
-     <body>
-        <h1>${defaultMeta.title}</h1>
-        <h2>${defaultMeta.description}</h2>
-    </body>
-</html> 
-`
+const _meta: Array<Meta> = [
+  { name: 'description', content: defaultMeta.title },
+  { name: 'og:title', content: frameConfig.title },
+  { name: 'og:description', content: frameConfig.description },
+  { name: 'og:image', content: frameConfig.image },
+  { name: 'og:url', content: frameConfig.url },
+  { name: 'og:type', content: 'website'},
+  { name: 'og:site_name', content: frameConfig.title},
+  { name: "fc:frame", content: farcasterMeta.frame},
+  { name: "fc:frame:image", content: defaultMeta.imgURL },
+  ...farcasterMeta.buttons.map((button: FrameButton) => button.getMeta()).flat()
+]
+
+// <!DOCTYPE html>
+// <html>
+//     <head>
+//         <title>${defaultMeta.title}</title>
+//         <meta property="og:title" content=${defaultMeta.title}>
+//         <meta property="og:description" content=${defaultMeta.description}>
+//         <meta property="og:image" content=${defaultMeta.imgURL}>
+//         <!------------------ CUSTOM META TAGS --------------------------------->
+//         <meta property="fc:frame" content="${farcasterMeta.frame}">
+//         <meta property="fc:frame:image" content=${defaultMeta.imgURL}>
+//         ${farcasterMeta.buttons.map((button: FrameButton) => button.getMeta()).join("\n")}
+//      </head>
+//      <body>
+//         <h1>${defaultMeta.title}</h1>
+//         <h2>${defaultMeta.description}</h2>
+//     </body>
+// </html>
+// `
 
 export default defineEventHandler(async (event) => {
   console.log("event", event)
-  console.log(_html)
+  console.log(_meta)
   return {
-    body: _html
+    _meta
   }
 })
